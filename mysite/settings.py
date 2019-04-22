0db7aefaea1ec0a3e8ac7330757caa829824b83a
namixkim <nlight815@gmail.com>
2019-04-22 18:11:43 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,4 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static') removed this because css is not working, 404 error
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
